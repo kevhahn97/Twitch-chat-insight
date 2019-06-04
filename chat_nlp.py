4524072f6b87510a1623c00b05492730ac13731d
5vermind <5vermind@gmail.com>
2019-06-04 19:05:42 +0900

    twitter(Okt)로 명사 빈도수 체크

--- chat_nlp.py
+++ chat_nlp.py
@@ -7,28 +7,20 @@ from collections import Counter
 
 def main():
 
-    with open('ambition2.json', 'r', encoding='utf-8-sig') as data_file:
+    with open('ambition3.json', 'r', encoding='utf-8-sig') as data_file:
         data = json.load(data_file)
 
     chats = ''
     chat_list = []
     nouns_file = "nouns.txt"
-    question_file = "question.txt"
-
-    quest_output_file = open(question_file, 'w', 1, "utf-8")
 
     for contents in data:
         chats = chats + ' ' + contents["contents"]
         chat_list.append(contents["contents"])
 
-    for ch in chat_list:
-        m = re.search('\w+?$', ch)
-        if m:
-            quest_output_file.write(' {}\n'.format(m.group()))
-    quest_output_file.close()
 
     nlpy = Twitter()
-    nouns = nlpy.morphs(chats)
+    nouns = nlpy.nouns(chats)
 
     count = Counter(nouns)
     tags_count = []
@@ -42,7 +34,7 @@ def main():
     output_file = open(nouns_file, 'w', 1, "utf-8")
 
     for tag in tags_count:
-       output_file.write(' {:<14}\t{}\n'.format(tag['tag'], tag['count']))
+        output_file.write(' {:<14}\t{}\n'.format(tag['tag'], tag['count']))
 
     output_file.close()
 
