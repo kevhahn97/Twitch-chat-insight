35a7ed3aa95506180a4e01f69ad535de9cd2c76b
openkmj <42940044+openkmj@users.noreply.github.com>
2019-06-04 21:10:52 +0900

    명사 빈도수 검사

--- chat_nlp.py
+++ chat_nlp.py
@@ -1,39 +1,44 @@
 import json
 import re
 
-from konlpy.tag import Twitter
+from konlpy.tag import Okt
 from collections import Counter
 
 
 def main():
 
-    with open('ambition3.json', 'r', encoding='utf-8-sig') as data_file:
+    filename = input("Input: ")
+    with open(filename+'.json', 'r', encoding='utf-8-sig') as data_file:
         data = json.load(data_file)
 
-    chats = ''
-    output = "output.txt"
+    nouns_file = filename+"_nouns.txt"
+
+    nouns = []
+    nlpy = Okt()
 
     for contents in data:
-        chats = chats + ' ' + contents["contents"]
-
-    nlpy = Twitter()
-    nouns = nlpy.nouns(chats)
+        nouns = nouns + nlpy.nouns(contents["contents"])
 
     count = Counter(nouns)
+    
+
+    
+
     tags_count = []
     tags = []
 
-    for n, c in count.most_common(100):
+    for n, c in count.most_common(50):
         dics = {'tag': n, 'count': c}
         tags_count.append(dics)
         tags.append(dics['tag'])
 
-    output_file = open(output, 'w', 1, "utf-8")
+    output_file = open(nouns_file, 'w', 1, "utf-8")
 
     for tag in tags_count:
+        print(' {:<14}\t{}\n'.format(tag['tag'], tag['count']))
         output_file.write(' {:<14}\t{}\n'.format(tag['tag'], tag['count']))
 
     output_file.close()
-
+    
 if __name__ == "__main__":
     main()
