381edbd4d0813eec3c9f4ee7621eebcb8e482644
5vermind <39900428+5vermind@users.noreply.github.com>
2019-06-04 20:49:08 +0900

    Update chat_nlp.py

--- chat_nlp.py
+++ chat_nlp.py
@@ -1,44 +1,39 @@
 import json
 import re
 
-from konlpy.tag import Okt
+from konlpy.tag import Twitter
 from collections import Counter
 
 
 def main():
 
-    filename = input("Input: ")
-    with open(filename+'.json', 'r', encoding='utf-8-sig') as data_file:
+    with open('ambition3.json', 'r', encoding='utf-8-sig') as data_file:
         data = json.load(data_file)
 
-    nouns_file = filename+"_nouns.txt"
-
-    nouns = []
-    nlpy = Okt()
+    chats = ''
+    output = "output.txt"
 
     for contents in data:
-        nouns = nouns + nlpy.nouns(contents["contents"])
+        chats = chats + ' ' + contents["contents"]
+
+    nlpy = Twitter()
+    nouns = nlpy.nouns(chats)
 
     count = Counter(nouns)
-    
-
-    
-
     tags_count = []
     tags = []
 
-    for n, c in count.most_common(50):
+    for n, c in count.most_common(100):
         dics = {'tag': n, 'count': c}
         tags_count.append(dics)
         tags.append(dics['tag'])
 
-    output_file = open(nouns_file, 'w', 1, "utf-8")
+    output_file = open(output, 'w', 1, "utf-8")
 
     for tag in tags_count:
-        print(' {:<14}\t{}\n'.format(tag['tag'], tag['count']))
         output_file.write(' {:<14}\t{}\n'.format(tag['tag'], tag['count']))
 
     output_file.close()
-    
+
 if __name__ == "__main__":
     main()
