3a691e5dd8b6c3cd273376be494b1a00c5139e4c
EungGu Yun <yuneg11@gmail.com>
2019-07-07 21:57:24 +0900

    Add duplication rule and refactoring codes

--- chat_cleaning.py
+++ chat_cleaning.py
@@ -1,9 +1,18 @@
 import codecs
 import re
+import time
 
+
+# Parameter
 length_minimum = 4
 set_minimum = 3
 whitelist_characters = r"([^!@#$%^&*(),.?\":{}|`~<>\[\]\-_=+\\/;'a-zA-Z0-9\u3131-\u318E\uAC00-\uD7A3\u1100-\u11f9\s]|http)"
+
+
+# Utility function
+def get_chats(filename):
+    with open(filename, 'r', encoding='utf-8-sig') as data_file:
+        return data_file.readlines()
 
 
 def is_simple_sentence(sentence):
@@ -24,46 +33,110 @@ def is_question(sentence):
     return True
 
 
-def is_contain_other_character(sentence, filter):
-    if filter.search(sentence) is None:
+def is_contain_other_character(sentence, character_filter):
+    if character_filter.search(sentence) is None:
         return False
     else:
         return True
 
 
-filename = input("filename: ")
-with open(filename, 'r', encoding='utf-8-sig') as data_file:
-    data = data_file.readlines()
+def is_duplicated(sentence, sentence_set):
+    if sentence in sentence_set:
+        return True
+    else:
+        return False
 
-chat_list = list()
-question_list = list()
-filter_regex = re.compile(whitelist_characters)
 
-for chatting in data:
-    if chatting[-1] == "\n":
-        chatting = chatting[:-1]
+def clean_chats(chats):
+    start_time = time.time()
 
-    # 1. check length
-    if len(chatting) < length_minimum:
-        continue
+    chat_set = set()
+    chat_list = list()
+    question_list = list()
 
-    # 2. check simple sentence
-    if is_simple_sentence(chatting):
-        continue
+    filter_regex = re.compile(whitelist_characters)
 
-    # 0. filter characters
-    if is_contain_other_character(chatting, filter_regex):
-        continue
+    processed_chat_count = [0, 0]  # chat, question
+    skipped_chat_count = [0, 0, 0, 0]  # filter, length, simple, duplicate
 
-    chat_list.append(chatting + "\n")
+    for chat in chats:
+        if chat[-1] == "\n":
+            chat = chat[:-1]
 
-    # 3. check question
-    if not is_question(chatting):
-        continue
+        # 1. check length
+        if len(chat) < length_minimum:
+            skipped_chat_count[1] += 1
+            continue
 
-    question_list.append(chatting + "\n")
+        # 2. check simple sentence
+        if is_simple_sentence(chat):
+            skipped_chat_count[2] += 1
+            continue
 
-with codecs.open('chat_' + filename, 'w', encoding='utf-8') as output_file:
-    output_file.writelines(chat_list)
-with codecs.open('question_' + filename, 'w', encoding='utf-8') as output_file:
-    output_file.writelines(question_list)
+        # 0. filter characters
+        if is_contain_other_character(chat, filter_regex):
+            skipped_chat_count[0] += 1
+            continue
+
+        # 3. check duplication
+        if is_duplicated(chat, chat_set):
+            skipped_chat_count[3] += 1
+            continue
+
+        chat_set.add(chat)
+        chat_list.append(chat + "\n")
+        processed_chat_count[0] += 1
+
+        # 4. check question
+        if not is_question(chat):
+            continue
+
+        question_list.append(chat + "\n")
+        processed_chat_count[1] += 1
+
+    print("Cleaning time: %.3f secs" % (time.time() - start_time))
+
+    return chat_list, question_list, [processed_chat_count, skipped_chat_count]
+
+
+def save_chats_and_questions(filename, chat_list, question_list):
+    filename_split = filename.split(".")
+    chat_filename = ".".join(filename_split[:-1]) + "_chat." + filename_split[-1]
+    question_filename = ".".join(filename_split[:-1]) + "_question." + filename_split[-1]
+
+    with codecs.open(chat_filename, 'w', encoding='utf-8') as output_file:
+        output_file.writelines(chat_list)
+    with codecs.open(question_filename, 'w', encoding='utf-8') as output_file:
+        output_file.writelines(question_list)
+
+
+def analyze_result(count):
+    processed = count[0]
+    skipped = count[1]
+
+    total_skipped = sum(skipped)
+    total = total_skipped + processed[0]
+
+    print("")
+    print("Total chats: %d" % total)
+    print("Skipped chats: %d (%.2f%%)" % (total_skipped, total_skipped / total * 100))
+    print("  0. Skipped chats by char filter : %d (%.2f%%)" % (skipped[0], skipped[0] / total_skipped * 100))
+    print("  1. Skipped chats by short length: %d (%.2f%%)" % (skipped[1], skipped[1] / total_skipped * 100))
+    print("  2. Skipped chats by simplicity  : %d (%.2f%%)" % (skipped[2], skipped[2] / total_skipped * 100))
+    print("  3. Skipped chats by duplication : %d (%.2f%%)" % (skipped[3], skipped[3] / total_skipped * 100))
+    print("Processed chats: %d (%.2f%%)" % (processed[0], processed[0] / total * 100))
+    print("  Processed normal chats: %d (%.2f%%)" % (processed[0] - processed[1], 100 - (processed[1] / processed[0] * 100)))
+    print("  Processed questions   : %d (%.2f%%)" % (processed[1], processed[1] / processed[0] * 100))
+
+
+# Main function
+def main():
+    filename = input("filename: ")
+    chats = get_chats(filename)
+    chat_list, question_list, count = clean_chats(chats)
+    save_chats_and_questions(filename, chat_list, question_list)
+    analyze_result(count)
+
+
+if __name__ == "__main__":
+    main()
